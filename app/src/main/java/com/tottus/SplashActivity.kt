e2d8d2430510ece238578097ba0854e360de2a42
William2406 <info.developer.william@gmail.com>
2020-11-01 12:18:17 -0500

    Add design splash

---
+++ app/src/main/java/com/tottus/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.tottus
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        GlobalScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
