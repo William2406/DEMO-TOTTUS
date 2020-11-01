ec98de09a6ec289db385d1164f97611100f7db19
William2406 <info.developer.william@gmail.com>
2020-11-01 16:16:59 -0500

    Add design register

--- app/src/main/java/com/tottus/LoginActivity.kt
+++ app/src/main/java/com/tottus/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.tottus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tottus.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initOnClick()
+    }
+
+    private fun initOnClick() {
+        binding.registerHereText.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
